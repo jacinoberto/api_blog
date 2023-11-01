a55d1145ceb9dcf3630df45beb85aab98fe1fc40
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2023-11-01 00:58:55 -0300

    Camada Controller criada

--- src/main/java/com/blog/application/repositories/PostRepository.java
+++ src/main/java/com/blog/application/repositories/PostRepository.java
@@ -2,8 +2,10 @@ package com.blog.application.repositories;
 
 import com.blog.application.models.PostModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PostRepository extends JpaRepository<PostModel, UUID> {
 }

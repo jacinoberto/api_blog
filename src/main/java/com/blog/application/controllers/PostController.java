e47a50d80d91fb88478cf0975c1ab69bcac6fcf6
Sala 307B <sala307b@ceines.com.br>
2023-11-06 21:10:53 -0300

    Classe model para usuario criada

--- src/main/java/com/blog/application/controllers/PostController.java
+++ src/main/java/com/blog/application/controllers/PostController.java
@@ -6,15 +6,13 @@ import com.blog.application.repositories.PostRepository;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PostController {
@@ -35,4 +33,5 @@ public class PostController {
     	
     	return ResponseEntity.ok().body(posts);
     }
+
 }
---
+++ src/main/java/com/blog/application/models/UsuarioModel.java
@@ -0,0 +1,68 @@
+package com.blog.application.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class UsuarioModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_usuario")
+    private UUID id;
+    private String nome;
+    private String senha;
+
+    public UsuarioModel() {
+    }
+
+    public UsuarioModel(UUID id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsuarioModel that = (UsuarioModel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
